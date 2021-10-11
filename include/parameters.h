d4dddaa248371b940eb0bd38cc6475b509ac46d8
Emre Albayrak <albayrak.emre@hotmail.com>
2021-10-11 17:48:24 +0300

    updated EEPROM addresses

--- include/parameters.h
+++ include/parameters.h
@@ -14,27 +14,27 @@ uint8_t microstep_coeff_x = 1; // Motor driver's microstep setting
 uint8_t microstep_coeff_y = 1; // Motor driver's microstep setting
 uint16_t max_speed_x = 150; // In units of mm/s
 uint16_t max_speed_y = 150; // In Units of mm/s
-uint32_t step_delay_speed_steady_x = 1000; // In units of microsecond
-uint32_t step_delay_speed_steady_y = 1000; // In units of microsecond
-uint32_t step_delay_speed_min_x = 5000; // In units of microsecond
-uint32_t step_delay_speed_min_y = 5000; // In units of microsecond
-uint32_t step_delay_instantaneous_x = 4000; // In units of microsecond
-uint32_t step_delay_instantaneous_y = 4000; // In units of microsecond
-uint32_t step_delay_acceleration_avg_x = 500; // In units of microsecond
-uint32_t step_delay_acceleration_avg_y = 500; // In units of microsecond
-uint32_t step_count_acceleration_x = 50; // Number of steps that acceleration going to applied on x-axis
-uint32_t step_count_acceleration_y = 50; // Number of steps that acceleration going to applied on y-axis
-uint16_t input_speed_steady_x = 100; // In units of mm/s
-uint16_t input_speed_steady_y = 100; // In units of mm/s
-uint8_t input_acceleration_x = 10; // In units of mm/(s^2)
-uint8_t input_acceleration_y = 10; // In units of mm/(s^2)
+uint32_t step_delay_speed_steady_x = 0; // In units of microsecond
+uint32_t step_delay_speed_steady_y = 0; // In units of microsecond
+uint32_t step_delay_speed_min_x = 8000; // In units of microsecond
+uint32_t step_delay_speed_min_y = 8000; // In units of microsecond
+uint32_t step_delay_instantaneous_x = 0; // In units of microsecond
+uint32_t step_delay_instantaneous_y = 0; // In units of microsecond
+uint32_t step_delay_acceleration_avg_x = 0; // In units of microsecond
+uint32_t step_delay_acceleration_avg_y = 0; // In units of microsecond
+uint32_t step_count_acceleration_x = 0; // Number of steps that acceleration going to applied on x-axis
+uint32_t step_count_acceleration_y = 0; // Number of steps that acceleration going to applied on y-axis
+uint16_t input_speed_steady_x = 0; // In units of mm/s
+uint16_t input_speed_steady_y = 0; // In units of mm/s
+uint8_t input_acceleration_x = 0; // In units of mm/(s^2)
+uint8_t input_acceleration_y = 0; // In units of mm/(s^2)
 char16_t driving_mechanism = '0'; // "0" -> Pulley System, "1" -> Lead Screw 
-float_t delta_t_x; // Time that acceleration is going to apply on x-axis
-float_t delta_t_y; // Time that acceleration is going to apply on y-axis
-uint32_t step_count_acceleration_calculated_x; //Memory for step_count_acceleration_x calculation 
-uint32_t step_count_acceleration_calculated_y; //Memory for step_count_acceleration_y calculation
-float_t system_cycle_linear_distance_x; // The varible keeps linear distance of one cycle for x-axis
-float_t system_cycle_linear_distance_y; // The varible keeps linear distance of one cycle for y-axis
+float_t delta_t_x = 0; // Time that acceleration is going to apply on x-axis
+float_t delta_t_y = 0; // Time that acceleration is going to apply on y-axis
+uint32_t step_count_acceleration_calculated_x = 0; //Memory for step_count_acceleration_x calculation 
+uint32_t step_count_acceleration_calculated_y = 0; //Memory for step_count_acceleration_y calculation
+float_t system_cycle_linear_distance_x = 0; // The varible keeps linear distance of one cycle for x-axis
+float_t system_cycle_linear_distance_y = 0; // The varible keeps linear distance of one cycle for y-axis
 float_t pi = 3.141592;
 
 
@@ -51,6 +51,8 @@ uint8_t address_input_speed_steady_y = 20;
 uint8_t address_input_acceleration_x = 22;
 uint8_t address_input_acceleration_y = 23;
 uint8_t address_driving_mechanism = 24;
+uint8_t address_step_delay_speed_min_x = 26;
+uint8_t address_step_delay_speed_min_y = 30;
 
 
 #endif PARAMETERS_H
